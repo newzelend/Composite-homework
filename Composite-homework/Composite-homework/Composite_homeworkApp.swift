23de8a4320ff0bd591bd412e46d088cc0bf17db0
newzelend <89086288+newzelend@users.noreply.github.com>
2021-11-15 18:40:22 +0300

    1

---
+++ Composite-homework/Composite-homework/Composite_homeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Composite_homeworkApp.swift
+//  Composite-homework
+//
+//  Created by Grisha Pospelov on 15.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Composite_homeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
